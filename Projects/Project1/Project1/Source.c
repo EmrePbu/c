022f561cc78e2a78eaa050fa1350cbc57cb83b14
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2022-02-13 12:04:43 +0300

    Line countingWord Counting

--- Projects/Project1/Project1/Source.c
+++ Projects/Project1/Project1/Source.c
@@ -1,6 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+#define		IN		1	/* inside a word */
+#define		OUT		0	/* outside a word */
+
+
 void main(void)
 {
 	/* Character Input and Output */
@@ -15,21 +19,44 @@ void main(void)
 	//	++nc;
 	//printf("%ld\n", nc);
 
-	// Character counting
+	//// Character counting
 	//double nc;
 	//for (nc = 0; getchar() != EOF; ++nc)
 	//	; // null statement
 	//printf("%.0f\n", nc);
 
-	// Line counting
-	int c, nl;
-	nl = 0;
-	while ((c = getchar()) != EOF)
-	{
-		if (c == '\n')
-		{
-			++nl;
-		}
-	}
-	printf("%d\n", nl);
+	//// Line counting
+	//int c, nl;
+	//nl = 0;
+	//while ((c = getchar()) != EOF)
+	//{
+	//	if (c == '\n')
+	//	{
+	//		++nl;
+	//	}
+	//}
+	//printf("%d\n", nl);
+
+	//// Word Counting
+	//int c, nl, nw, nc, state;
+	//state = OUT;
+	//nl = nw = nc = 0;
+	//while ((c = getchar()) != EOF)
+	//{
+	//	++nc;
+	//	if (c == ' \n')
+	//	{
+	//		++nl;
+	//	}
+	//	if (c == ' ' || c == '\n' || c == '\t')
+	//	{
+	//		state = OUT;
+	//	}
+	//	else if (state == OUT)
+	//	{
+	//		state = IN;
+	//		++nw;
+	//	}
+	//}
+	//printf("%d %d %d \n", nl, nw, nc);
 }
