08b071e6bc9c6306b45f333d9deabff4e97a7c5f
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2022-02-15 21:03:30 +0300

    1.9 Character Arrays

--- Projects/Project1/Project1/Source.c
+++ Projects/Project1/Project1/Source.c
@@ -1,7 +1,49 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define		MAXLINE		1000
+
 #include <stdio.h>
+
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
 
 void main(void)
 {
+	int len;				// Currrent line length
+	int max;				// Maximum length seen so far
+	char line[MAXLINE];		// Current input line
+	char longest[MAXLINE];	// Longest line saved here
+
+	max = 0;
+	while ((len = getline(line, MAXLINE)) > 0)
+		if (len > max)
+		{
+			max = len;
+			copy(longest, line);
+		}
+	if (max > 0)
+		printf("%s", longest);
+	return 0;
+}
+
+int getline(char line[], int maxline)
+{
+	int c, i;
+	for (i = 0; i < maxline - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		line[i] = c;
+	if (c == '\n')
+	{
+		line[i] = c;
+		++i;
+	}
+	line[i] = '\0';
+	return i;
+}
+
+void copy(char to[], char from[])
+{
+	int i;
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+		++i;
 
 }
