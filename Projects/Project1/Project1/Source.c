68ab2a1b3788a826c8e7607dff4a89e1374946ea
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2022-02-19 20:19:38 +0300

    __LINE__ __FILE__ __TIME__ __TIMESTAMP__ __DATE__

--- Projects/Project1/Project1/Source.c
+++ Projects/Project1/Project1/Source.c
@@ -3,7 +3,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void func(int a)
+{
+	printf("%s fonksiyonuna gelen paramatre degeri : %d", __func__, a);
+}
+
+
 int main(void)
 {
-	
+	int i_val;
+	printf("Bir tam sayi giriniz : ");
+	(void)scanf("%d", &i_val);
+
+	if (i_val == 0)
+	{
+		//printf("Source.c dosyasinin %d. satirinda i_val degeri 0 olarak farkedildi.\n", __LINE__);
+
+		//printf("%s", __FILE__);
+
+		//printf("%s\n", __TIME__);
+		//printf("%s\n", __TIMESTAMP__);
+		//printf("%s\n", __DATE__);
+
+		func(30);
+
+		abort();
+	}
 }
