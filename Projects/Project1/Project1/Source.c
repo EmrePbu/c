f99c2313d07d7e9d7e64fe1d55a1f10e2d0ce7a1
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2022-02-20 15:32:18 +0300

    Save

--- Projects/Project1/Project1/Source.c
+++ Projects/Project1/Project1/Source.c
@@ -6,35 +6,10 @@
 
 int main(void)
 {
-	for (;;)
-	{
-		switch (rand() % 7 + 1)
-		{
-		case 1:
-			printf("Pazartesi\n");
-			break;
-		case 2:
-			printf("Sali\n");
-			break;
-		case 3:
-			printf("Carsamba\n");
-			break;
-		case 4:
-			printf("Persembe\n");
-			break;
-		case 5:
-			printf("Cuma\n");
-			break;
-		case 6:
-			printf("Cumartesi\n");
-			break;
-		case 7:
-			printf("Pazar\n");
-			break;
-		default:
-			break;
-		}
-		if (getchar() == 'q')
-			break;
-	}
+	char c = 178;
+	printf("%d,%c\n", c, c);
+	if (c == 178)
+		printf("dogru");
+	else
+		printf("yanlis");
 }
