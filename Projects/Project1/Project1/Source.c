b41b4819c13135dd3a7714935801c6f4aa143aba
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2022-02-24 21:15:24 +0300

    Find Pi Number

--- Projects/Project1/Project1/Source.c
+++ Projects/Project1/Project1/Source.c
@@ -1,12 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define     NPOINTS     100000
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <conio.h>
-#include <time.h>
+#include "utils.h"
 
 
-int main(void)
-{
+int main() {
 
+	int insideCount = 0;
+	for (int i = 0; i < NPOINTS; ++i)
+	{
+		double dX = (double)rand() / RAND_MAX;
+		double dY = (double)rand() / RAND_MAX;
+		if (dX * dX + dY * dY <= 1.)
+		{
+			++insideCount;
+		}
+	}
+
+	printf("pi = %.12f\n", 4. * insideCount / NPOINTS);
+
+	return (0);
 }
--- Projects/Project1/Project1/utils.h
+++ Projects/Project1/Project1/utils.h
@@ -2,9 +2,33 @@
 #ifndef _UTILS_H
 #define _UTILS_H
 
+#define PUBLIC
+
+PUBLIC int a = 12;
+static int b = 10;
+
 struct Data
 {
-	int a, b, c;
+	struct Data1
+	{
+		struct Data12
+		{
+			struct Data13
+			{
+				struct Data14
+				{
+					struct Data15
+					{
+						struct Data16
+						{
+							int a;
+						};
+					};
+				};
+			};
+		};
+	};
 };
+
 
 #endif // !_UTILS_H
